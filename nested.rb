e2106469a827e870168455d98f18cb5d38766149
Tracy Ngo <tngo1206@gmail.com>
2019-02-14 04:19:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
         }
      }
 
-    return programmer_hash[:yukihiro_matsumoto] = {
+    return programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
