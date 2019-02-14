fe24f9573d1ef0f548382b55cb0c96cc027208d4
Tracy Ngo <tngo1206@gmail.com>
2019-02-14 04:22:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:la]
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 
 end
